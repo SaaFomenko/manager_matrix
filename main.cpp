6ba5631e47284b0c8ac6845b6011ab317e4ad787
SaaFomenko <s.fomenko@list.ru>
2023-05-23 17:52:48 +0300

    Change library matrix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-#include <iostriam>
+#include <iostream>
+#include "lib/matrix/matrix.cpp"
 
 
 int main()
 {
-	pass
+	try
+	{
+		Array<int> arr1(4);
+
+		std::cout << "Create arr1.\n";
+		arr1[0] = 3;
+  
+		std::cout << "arr1[0] = " << arr1[0] << "\n";
+		std::cout << "arr1[1] = " << arr1[1] << "\n";
+	}
+	catch (std::exception e)
+	{
+		std::cout << e.what() << "\n";
+	}
 
 	return 0;
 }
